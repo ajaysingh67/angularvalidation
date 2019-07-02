11e8c7fcef53fe067928236f027dc7e01562af5e
unknown <ajay@cashaa.com>
2019-07-03 01:04:43 +0530

    material

---
+++ materialsidebar/main.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRouteSnapshot, NavigationEnd, NavigationError } from '@angular/router';
+
+import { Title } from '@angular/platform-browser';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+
+@Component({
+  selector: 'jhi-main',
+  templateUrl: './main.component.html'
+})
+export class JhiMainComponent implements OnInit {
+
+  showFiller = false;
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches)
+    );
+
+  constructor(private titleService: Title, private router: Router,private modalService: NgbModal,private breakpointObserver: BreakpointObserver) {}
+
+  private getPageTitle(routeSnapshot: ActivatedRouteSnapshot) {
+    let title: string = routeSnapshot.data && routeSnapshot.data['pageTitle'] ? routeSnapshot.data['pageTitle'] : 'tdemoApp';
+    if (routeSnapshot.firstChild) {
+      title = this.getPageTitle(routeSnapshot.firstChild) || title;
+    }
+    return title;
+  }
+
+  ngOnInit() {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.titleService.setTitle(this.getPageTitle(this.router.routerState.snapshot.root));
+      }
+      if (event instanceof NavigationError && event.error.status === 404) {
+        this.router.navigate(['/404']);
+      }
+    });
+  }
+
+  open(content) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+}
